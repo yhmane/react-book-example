19d11bd1cf550bd0d149663b1af070dd4995f3e7
yhmane <hwang9yh@gmail.com>
2022-01-22 10:49:05 +0900

    state 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import MyComponent from './MyComponent'
+import Counter from './Counter'
 
 const App = () => {
   return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
+    <Counter/>
   );
 };
 
